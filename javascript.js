3cabff13fbcb8cb28d9838cc08cf1d2465c4c01e
MuskanKumari23 <muskankumari92084@Gmail.com>
2025-09-19 15:45:01 +0530

    /

---
+++ javascript.js
@@ -0,0 +1,13 @@
+var unit_price=29;
+var qantiy=30;
+var amount = unit_price * qantity;
+var discount = 0.1*amount;
+
+
+ if (amount>=1000){
+    console.log("discount");
+  } else {
+        console.log("no discount");
+
+    }
+
